c75abd151ae34214644fcdd036e2588fc9f89aae
Maximus <>
2016-10-06 11:20:01 +0200

    changed constructor in room class

--- room.py
+++ room.py
@@ -4,19 +4,28 @@
 
 class Room():
 	def __init__(self, room_number = None, room_name = None, room_available = None, room_size = None):
-		self.room_number = 0;
-		self.room_name = "Empty Room"
-		self.room_available = True
-		self.room_size = -1
+
+		if room_number is None and room_name is None and room_available is None and room_size is None:
+			self.room_number = 0;
+			self.room_name = "Empty Room"
+			self.room_available = True
+			self.room_size = -1
+
+		else:
+			self.room_number = room_number
+			self.room_name = room_name
+			self.room_available = room_available
+			self.room_size = room_size		
 		pass
 
+		
 	def __del__(self):
 		pass
 
 	def addUser(self, user = None):
 		#branch if user is object or number
 		pass
-		
+
 	
 	def delUser(self, user = None):
 		#branch if user is object or number
